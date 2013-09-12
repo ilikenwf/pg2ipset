839dbd2cfdb0efcb93cfad6f04257c3c19b80dd3
mynamewastaken <mynamewastaken+git@gmail.com>
2013-09-12 12:11:48 -0500

    updated to remove deprecated syntax

--- ipset-update.sh
+++ ipset-update.sh
@@ -22,9 +22,9 @@ do
         ipset create -exist $list hash:net family inet maxelem 4294967295
         ipset create -exist $list-TMP hash:net family inet maxelem 4294967295
         ipset flush $list-TMP &> /dev/null
-        zcat $LISTDIR/$list.gz | pg2ipset - - $list-TMP | ipset -R
-        ipset -W $list $list-TMP
-        ipset -X $list-TMP
+        zcat $LISTDIR/$list.gz | pg2ipset - - $list-TMP | ipset restore
+        ipset swap $list $list-TMP
+        ipset destroy $list-TMP
 done
 
 if [ eval $(curl -L http://ipinfodb.com/country_query.php?country=AF,AE,IR,IQ,TR,CN,SA,SY,RU,UA,HK,ID,KZ,KW,LY -o /tmp/countries.txt) ]; then
@@ -37,15 +37,15 @@ echo "Importing country blocks..."
 ipset create -exist countries hash:net maxelem 4294967295
 ipset create -exist countries-TMP hash:net maxelem 4294967295
 ipset flush countries-TMP &> /dev/null
-awk '!x[$0]++' $LISTDIR/countries.txt | sed -e 's/^/\-A\ \-exist\ countries\ /' | ipset -R
-ipset -W countries countries-TMP
-ipset -X countries-TMP
+awk '!x[$0]++' $LISTDIR/countries.txt | sed -e 's/^/\-A\ \-exist\ countries\ /' | ipset restore
+ipset swap countries countries-TMP
+ipset destroy countries-TMP
 
 
 echo "Importing Custom blocks..."
 ipset create -exist Custom hash:net maxelem 4294967295
 ipset create -exist Custom-TMP hash:net maxelem 4294967295
 ipset flush Custom-TMP &> /dev/null
-awk '!x[$0]++' $LISTDIR/Custom.txt | sed -e 's/^/\-A\ \-exist\ Custom\ /' | ipset -R
-ipset -W Custom Custom-TMP
-ipset -X Custom-TMP
+awk '!x[$0]++' $LISTDIR/Custom.txt | sed -e 's/^/\-A\ \-exist\ Custom\ /' | ipset restore
+ipset swap Custom Custom-TMP
+ipset destroy Custom-TMP
