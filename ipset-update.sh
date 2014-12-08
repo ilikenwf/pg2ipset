c2cdd492d460fe341241dbbf78aac7de88de6e8f
Matt Parnell <parwok@gmail.com>
2014-12-08 12:19:52 -0600

    code cleanup

--- ipset-update.sh
+++ ipset-update.sh
@@ -39,31 +39,31 @@ IPTABLES=$(iptables-save)
 
 importList(){
 	if [ -f $LISTDIR/$1.txt ] || [ -f $LISTDIR/$1.gz ]; then
-		echo "Importing $1 blocks..."
-		ipset create -exist $1 hash:net maxelem 4294967295
-		ipset create -exist $1-TMP hash:net maxelem 4294967295
-		ipset flush $1-TMP &> /dev/null
-		
-		#the second param determines if we need to use zcat or not
-		if [ $2 = 1 ]; then
-		  zcat $LISTDIR/$1.gz | grep  -v \# | grep -v ^$ | pg2ipset - - $listname-TMP | ipset restore
-		else
-		  awk '!x[$0]++' $LISTDIR/$1.txt | grep  -v \# | grep -v ^$ | ipset restore
-		fi
-		
-		ipset swap $1 $1-TMP
-		ipset destroy $1-TMP
-		
-		# if the first rule exists already, it is likely the others do too
-		# only create if they don't exist
-		if ! echo $IPTABLES|grep -q "\-A\ INPUT\ \-m\ set\ \-\-match\-set\ $1\ src\ \-\j\ DROP"; then
-		  iptables -A INPUT -m set --match-set $1 src -j DROP
-		  iptables -A FORWARD -m set --match-set $1 src -j DROP
-		  iptables -A FORWARD -m set --match-set $1 dst -j REJECT
-		  iptables -A OUTPUT -m set --match-set $1 dst -j REJECT
-		fi
+	  echo "Importing $1 blocks..."
+	  
+	  ipset create -exist $1 hash:net maxelem 4294967295
+	  ipset create -exist $1-TMP hash:net maxelem 4294967295
+	  ipset flush $1-TMP &> /dev/null
+	  
+	  #the second param determines if we need to use zcat or not
+	  if [ $2 = 1 ]; then
+		zcat $LISTDIR/$1.gz | grep  -v \# | grep -v ^$ | pg2ipset - - $listname-TMP | ipset restore
+	  else
+		awk '!x[$0]++' $LISTDIR/$1.txt | grep  -v \# | grep -v ^$ | ipset restore
+	  fi
+	  
+	  ipset swap $1 $1-TMP
+	  ipset destroy $1-TMP
+	  
+	  # only create if the iptables rules don't already exist
+	  if ! echo $IPTABLES|grep -q "\-A\ INPUT\ \-m\ set\ \-\-match\-set\ $1\ src\ \-\j\ DROP"; then
+		iptables -A INPUT -m set --match-set $1 src -j DROP
+		iptables -A FORWARD -m set --match-set $1 src -j DROP
+		iptables -A FORWARD -m set --match-set $1 dst -j REJECT
+		iptables -A OUTPUT -m set --match-set $1 dst -j REJECT
+	  fi
 	else
-		echo "List $1.txt does not exist."
+	  echo "List $1.txt does not exist."
 	fi
 }
 
@@ -73,29 +73,29 @@ if [ $ENABLE_BLUETACK = 1 ]; then
 	# pg2ipset to be inserted
 	i=1
 	for list in ${BLUETACK[@]}; do
-			listname="bluetack$i"
-		
-			if [ eval $(wget --quiet -O /tmp/$listname.gz http://list.iblocklist.com/?list=$list&fileformat=p2p&archiveformat=gz) ]; then
-					mv /tmp/$listname.gz $LISTDIR/$listname.gz
-			else
-					echo "Using cached list for $list."
-			fi
-			
-			echo "Importing bluetack list $list..."
-
-			importList $listname 1
-			
-			i=$((i+1))
+	  listname="bluetack$i"
+  
+	  if [ eval $(wget --quiet -O /tmp/$listname.gz http://list.iblocklist.com/?list=$list&fileformat=p2p&archiveformat=gz) ]; then
+		mv /tmp/$listname.gz $LISTDIR/$listname.gz
+	  else
+		echo "Using cached list for $list."
+	  fi
+	  
+	  echo "Importing bluetack list $list..."
+  
+	  importList $listname 1
+	  
+	  i=$((i+1))
 	done
 fi
 
 if [ $ENABLE_COUNTRY = 1 ]; then
 	# get the country lists and cat them into a single file
 	for country in ${COUNTRIES[@]}; do
-			if [ eval $(wget --quiet -O /tmp/$country.txt http://www.ipdeny.com/ipblocks/data/countries/$country.zone) ]; then
-					cat /tmp/$country.txt >> $LISTDIR/countries.txt
-					rm /tmp/$country.txt
-			fi
+	  if [ eval $(wget --quiet -O /tmp/$country.txt http://www.ipdeny.com/ipblocks/data/countries/$country.zone) ]; then
+		cat /tmp/$country.txt >> $LISTDIR/countries.txt
+		rm /tmp/$country.txt
+	  fi
 	done
 	
 	importList "countries" 0
@@ -105,12 +105,12 @@ fi
 if [ $ENABLE_TORBLOCK = 1 ]; then
 	# get the tor lists and cat them into a single file
 	for ip in $(ip -4 -o addr | awk '!/^[0-9]*: ?lo|link\/ether/ {gsub("/", " "); print $4}'); do
-			for port in ${PORTS[@]}; do
-				if [ eval $(wget --quiet -O /tmp/$port.txt https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$ip&port=$port) ]; then
-						cat /tmp/$port.txt >> $LISTDIR/tor.txt
-						rm /tmp/$port.txt
-				fi
-			done
+	  for port in ${PORTS[@]}; do
+		if [ eval $(wget --quiet -O /tmp/$port.txt https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$ip&port=$port) ]; then
+		  cat /tmp/$port.txt >> $LISTDIR/tor.txt
+		  rm /tmp/$port.txt
+		fi
+	  done
 	done 
 	
 	importList "tor" 0
